d1a35c344991b09893ad06a0d202bb3c3bb2f23c
aniketvishwakarma-beast <aniket.vishwakarm@ccf.mu.ac.in>
2026-07-17 13:53:59 +0530

    Fix NEP module import package resolution collision

--- APP.py
+++ APP.py
@@ -185,7 +185,6 @@ h1, h2, h3, h4, h5, h6 {
 current_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.join(current_dir, 'BAPRG'))
 sys.path.append(os.path.join(current_dir, 'BSCPRG'))
-sys.path.append(os.path.join(current_dir, 'NEP'))
 
 # --- Sidebar Content ---
 st.sidebar.markdown(
